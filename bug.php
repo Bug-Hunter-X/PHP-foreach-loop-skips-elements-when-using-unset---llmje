1f77bbae1d04031a2cc5b06d9c0cea014a7c487e
BugHunter-X <askzenai@gmail.com>
2025-01-09 12:02:05 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,19 @@
+function foo(array $arr) {
+  foreach ($arr as $key => $value) {
+    if ($value === 'bar') {
+      unset($arr[$key]);
+    }
+  }
+  return $arr;
+}
+
+$arr = ['foo', 'bar', 'baz'];
+$result = foo($arr);
+print_r($result); // Output: Array ( [0] => foo [2] => baz )
+
+$arr = ['foo' => 'foo', 'bar' => 'bar', 'baz' => 'baz'];
+$result = foo($arr);
+print_r($result); // Output: Array ( [foo] => foo [baz] => baz )
+
+// The problem is that when you unset an element from an array during a foreach loop, the loop might skip elements.
+// The correct way to do it is to use a for loop, or create a new array without the elements to be removed.
